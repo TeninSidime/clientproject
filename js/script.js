8a51986415623fb5561ea17219d9a147d597a1a9
Tenin Sidime <sidime.tenin@gmail.com>
2019-05-08 22:07:00 +0000

    javascript and homepage vs info navigation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 /* global $ */
 
-console.log('hi');
-
 $('h1').click(function(){
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
+});
+
+
+
+$("#home").click(function () {
+    $('.classofdivcontaininginfo').show();
+    $('.classofdivcontainigwhatyouwannahide').hide();
+});
+
+
+$("#info").click(function () {
+    console.log('heyo');
+    $('.mattexample').show();
+    $('.homepage').hide();
+});
+
+
+
+$("#order").click(function () {
+    $('.classofdivcontaininginfo').show();
+    $('.homepage').hide();
 });
