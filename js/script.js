fc16d9a3be7b4aeba08ae8a577f6b31792288687
Tenin Sidime <sidime.tenin@gmail.com>
2019-05-15 21:16:48 +0000

    api with error that dosen't display

--- js/script.js
+++ js/script.js
@@ -6,22 +6,47 @@ $('h1').click(function(){
 });
 
 
+function showHomepage() {
+    $('#homepage').show();
+    $('#infopage').hide();
+    $('#orderpage').hide();
+}
+
+// when the page is ready... make sure homepage shows up but nothing else
+$(function () {
+    showHomepage();
+})
 
 $("#home").click(function () {
-    $('.classofdivcontaininginfo').show();
-    $('.classofdivcontainigwhatyouwannahide').hide();
+    showHomepage();
+});
+
+$("#getData").click(function (){
+    var zipString = $("#zip").val();
+    console.log(zipString);
+    if(zipString.length != 5) {
+        alert("invalid zip code, I only accept 5 digit zips, I'm just not that clever sorry!");
+    } else {
+        // https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b6907d289e10d714a6e88b30761fae22
+        
+        var apiURL = 'https://api.openweathermap.org/data/2.5/weather?zip=' + zipString + ',us&appid=390848c94df3318563d07d23be4f4da3';
+        console.log(apiURL);
+    }
+    // 40.7128° N, 74.0060°
+    
 });
 
 
 $("#info").click(function () {
-    console.log('heyo');
-    $('.mattexample').show();
-    $('.homepage').hide();
+    $('#homepage').hide();
+    $('#infopage').show();
+    $('#orderpage').hide();
 });
 
 
 
 $("#order").click(function () {
-    $('.classofdivcontaininginfo').show();
-    $('.homepage').hide();
+    $('#homepage').hide();
+    $('#infopage').hide();
+    $('#orderpage').show();
 });
